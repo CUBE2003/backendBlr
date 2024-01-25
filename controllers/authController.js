028298f394675d6ecf4ecfa4367d0d7783d72a60
cube2003 <ujjvalk2003@outlook.com>
2024-01-25 18:24:06 +0530

    fifth commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,47 +39,36 @@ module.exports = {
     },
 
     loginUser: async (req, res) => {
-
         try {
-
-            const user = await User.findOne({ email: req.body.email },
-
-                { _v: 0, createdAt: 0, updatedAt: 0, skills: 0, email: 0 });
-
+            const user = await User.findOne({ email: req.body.email }, { _v: 0, createdAt: 0, updatedAt: 0, skills: 0, email: 0 });
+    
             if (!user) {
-
                 return res.status(400).json({
-
                     message: 'User not found'
-
                 });
-
-                const decryptedPassword = CryptoJs.AES.decrypt(user.password, process.env.SECRET);
-                const depassword = decryptedPassword.toString(CryptoJs.enc.Utf8);
-
-                if (depassword !== req.body.password) {
-                    return res.status(400).json({ message: 'invalid password' })
-                }
-
-                const userToken = jwt.sign({
-                    id: user._id,
-                    isAdmin: user.isAdmin,
-                    isAgent: user.isAgent,
-                    uid: user._id,
-                }, process.env.JWT_SEC,{expiresIn:'21d'});
-
-                const{password,isAdmin,...others}=user._doc
-
-                res.status(200).json({...others,userToken})
-                
-
             }
+    
+            const decryptedPassword = CryptoJs.AES.decrypt(user.password, process.env.SECRET);
+            const depassword = decryptedPassword.toString(CryptoJs.enc.Utf8);
+    
+            if (depassword !== req.body.password) {
+                return res.status(400).json({ message: 'Invalid password' });
+            }
+    
+            const userToken = jwt.sign({
+                id: user._id,
+                isAdmin: user.isAdmin,
+                isAgent: user.isAgent,
+                uid: user._id,
+            }, process.env.JWT_SEC, { expiresIn: '21d' });
+    
+            const { password, isAdmin, ...others } = user._doc;
+    
+            res.status(200).json({ ...others, userToken });
+    
         } catch (error) {
-            res.status(500).json({error:'an error occured while logging'})
+            res.status(500).json({ error: 'An error occurred while logging in' });
         }
-
-
-
-
     }
+    
 }
